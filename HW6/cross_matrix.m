17312ea2f1e47eaf180cf5878d327db64f219a0b
Yuhao <zyh19952015@gmail.com>
2020-10-12 15:51:45 -0500

    Revert "Add GCon_DP2"
    This reverts commit 635331db69268118d45c8f5e1b4bc207c251c314.

--- HW6/cross_matrix.m
+++
@@ -1,8 +0,0 @@
-function a_tilde=cross_matrix(a)
-% This function convert a vector "a" into its cross-product matrix form
-% a_tilde.
-
-a_tilde = [0 -a(3) a(2);...
-           a(3) 0 -a(1);...
-           -a(2) a(1) 0];
-end
