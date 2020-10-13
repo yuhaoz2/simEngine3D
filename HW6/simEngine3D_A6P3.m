f927ac46de5a7ec1160ed6bca21e9d235b69f7a3
Yuhao <zyh19952015@gmail.com>
2020-10-13 18:26:26 -0500

    Add report

--- HW6/simEngine3D_A6P3.m
+++ HW6/simEngine3D_A6P3.m
@@ -59,4 +59,43 @@ r_Q = zeros(3,length(states));
 dr_Q = zeros(3,length(states));
 ddr_Q = zeros(3,length(states));
 
+for k = 1:length(states)
+    % compute orientation matrix
+    p_i     = states{k}.q(4:7); 
+    dp_i    = states{k}.dq(4:7); 
+    ddp_i   = states{k}.ddq(4:7); 
+    A_i = p2A(p_i);
+    B = pa2B(p_i,s_i_Q_bar);
+    r_Q(:,k) = r_i(:,k) + A_i*s_i_Q_bar;
+    dr_Q(:,k) = dr_i(:,k) + B*dp_i;
+    ddr_Q(:,k) = ddr_i(:,k) + B*ddp_i + pa2B(dp_i,s_i_Q_bar)*dp_i;
+end
+
+figure
+subplot(3,1,1)
+hold on
+plot(t,r_Q)
+title('Position analysis of point Q')
+xlabel('Time (sec)')
+ylabel('Position in G-LF (m)')
+legend('rQ_X','rQ_Y','rQ_Z')
+hold off
+
+subplot(3,1,2)
+hold on
+plot(t,dr_Q)
+title('Velocity analysis of point Q')
+xlabel('Time (sec)')
+ylabel('Velocity in G-LF (m/s)')
+legend('drQ_X','drQ_Y','drQ_Z')
+hold off
+
+subplot(3,1,3)
+hold on
+plot(t,ddr_Q)
+title('Acceleration analysis of point Q')
+xlabel('Time (sec)')
+ylabel('Acceleration in G-LF (m/s^2)')
+legend('ddrQ_X','ddrQ_Y','ddrQ_Z')
+hold off
 
