f75dc8f3aec068582b9b76ab5eade12394d7b037
Yuhao <zyh19952015@gmail.com>
2020-10-22 01:11:19 -0500

    Add some comments

--- HW7/simEngine3D_A7P1.m
+++ HW7/simEngine3D_A7P1.m
@@ -6,20 +6,24 @@ clear
 % build the model
 sys = simEngine3D("revJoint"); % revJoint.m is the model info file
 
+% Assign start time, ending time and time step
 t_start = 0;
 t_end = 10;
 t_step = 1e-3;
 
+% Perform the inverse dynamic analysis
 torques = sys.inverse_dynamics(t_start,t_step,t_end);
 
 t = t_start:t_step:t_end;
 
 torque =zeros(3,length(t));
 
+% Find the torque on the driving constraint
 for i = 1: length(t)
     torque(:,i) = torques{i}{6,1};
 end
 
+% Plot the torque
 plot(t,torque);
 title('Torque applied to pendulum');
 xlabel('Time (sec)');
